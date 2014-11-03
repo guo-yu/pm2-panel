01a27a75a94c05fa380e0a0566951e97e1310364
Guo Yu <i@guoyu.me>
2014-11-03 17:21:53 +0800

    0.1: Reorganize files

--- app.simple.js
+++
@@ -1,7 +0,0 @@
-var server = require('./server');
-
-// init a new server running on port 3000
-new server({
-    name: 'pm2-panel',
-    desc: 'the missing web/mobile panel of pm2 monitor'
-}).run();
